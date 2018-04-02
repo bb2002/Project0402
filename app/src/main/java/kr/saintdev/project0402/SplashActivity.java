015f4d89d66a3ffdb03e1374c6d4db73df70ec3e
5252bb <jmjm0128>
2018-04-02 11:41:39 +0900

    0402 1141 / splash

--- app/src/main/java/kr/saintdev/project0402/SplashActivity.java
+++ app/src/main/java/kr/saintdev/project0402/SplashActivity.java
@@ -3,7 +3,7 @@ package kr.saintdev.project0402;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
